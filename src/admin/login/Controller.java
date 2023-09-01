eae092f257bc945c9465fbc2013d201a0fcaed87
stuti2011shah <stuti2011shah@gmail.com>
2023-09-01 19:26:04 +0530

    Test Commit - 2

--- src/admin/login/Controller.java
+++ src/admin/login/Controller.java
@@ -3,6 +3,10 @@ package admin.login;
 import javafx.fxml.FXML;
 
 public class Controller {
-    public void image(){}
-    public void login(){}
+    public void image(){
+
+    }
+    public void login(){
+
+    }
 }
